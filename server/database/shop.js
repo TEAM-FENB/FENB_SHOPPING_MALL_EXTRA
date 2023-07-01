d3b82b78e3a33276c894595cb5f462d0cfe56ba1
LEE DONG GYU <tenenger7125@gmail.com>
2023-07-01 17:05:57 +0900

    refactor: remove option because of mongoose lastest version default option

--- server/database/shop.js
+++ server/database/shop.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const mongooseConnect = async callback => {
   try {
     await mongoose.connect(process.env.MONGO_DB_URL, {
-      useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     callback();
