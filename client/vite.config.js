8b9968705c3e0de6fd442c9d6c8fed13b348913b
LEE DONG GYU <tenenger7125@gmail.com>
2023-07-06 21:05:05 +0900

    config: update proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: `http://localhost:${PORT}`,
+        target: `http://ec2-3-36-56-62.ap-northeast-2.compute.amazonaws.com:${PORT}/`,
         changeOrigin: true,
       },
     },
