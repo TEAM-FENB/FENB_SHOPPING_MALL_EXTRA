7de7301cddaf608ebdbee66d0d8495c47c94ab1b
LEE DONG GYU <tenenger7125@gmail.com>
2023-07-06 22:27:32 +0900

    config: add https proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,18 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import jsconfigPaths from 'vite-jsconfig-paths';
 
+const PORT = 8000;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), jsconfigPaths()],
+  server: {
+    proxy: {
+      '/api': {
+        target: `http://ec2-3-36-56-62.ap-northeast-2.compute.amazonaws.com:${PORT}/`,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
