a506d8f2199737c18359d122990d35bef996f248
LEE DONG GYU <tenenger7125@gmail.com>
2023-07-06 21:43:52 +0900

    config: update aws ec2 server url

--- client/src/api/fetch.js
+++ client/src/api/fetch.js
@@ -1,22 +1,24 @@
 import axios from 'axios';
 
+const API_URL = 'http://ec2-3-36-56-62.ap-northeast-2.compute.amazonaws.com:8000';
+
 export const fetchProducts = async () => {
-  const { data } = await axios.get('/api/products');
+  const { data } = await axios.get(`${API_URL}/api/products`);
   return data;
 };
 
 export const fetchPageProducts = async (page, pageSize) => {
-  const { data } = await axios.get(`/api/products/pages/${page}?pageSize=${pageSize}`);
+  const { data } = await axios.get(`${API_URL}/api/products/pages/${page}?pageSize=${pageSize}`);
   return data;
 };
 
 export const fetchFilteredProducts = search => async () => {
-  const { data } = await axios.get(`/api/products${search}`);
+  const { data } = await axios.get(`${API_URL}/api/products${search}`);
   return data;
 };
 
 export const fetchSlides = async () => {
-  const { data } = await axios.get('/api/slides');
+  const { data } = await axios.get(`${API_URL}/api/slides`);
   return data;
 };
 
@@ -25,17 +27,17 @@ export const signOut = async () => {
 };
 
 export const requestVerify = async () => {
-  const { data } = await axios.get('/api/auth/verify');
+  const { data } = await axios.get(`${API_URL}/api/auth/verify`);
   return data;
 };
 
 export const fetchFavorites = async () => {
-  const { data } = await axios.get('/api/favorites/me');
+  const { data } = await axios.get(`${API_URL}/api/favorites/me`);
   return data;
 };
 
 export const fetchCarts = async () => {
-  const { data } = await axios.get('/api/carts/me');
+  const { data } = await axios.get(`${API_URL}/api/carts/me`);
   return data;
 };
 
@@ -45,22 +47,22 @@ export const fetchAddresses = async () => {
 };
 
 export const fetchCoupons = async () => {
-  const { data } = await axios.get('/api/coupons');
+  const { data } = await axios.get(`${API_URL}/api/coupons`);
   return data;
 };
 
 export const applyCoupon = async id => {
-  const { data } = await axios.get(`/api/order/coupons/${id}`);
+  const { data } = await axios.get(`${API_URL}/api/order/coupons/${id}`);
   return data;
 };
 
 export const fetchHistory = async () => {
-  const { data } = await axios.get('/api/order/history');
+  const { data } = await axios.get(`${API_URL}/api/order/history`);
   return data;
 };
 
 export const signIn = async ({ email, password }) => {
-  const { data } = await axios.post('/api/auth/signin', {
+  const { data } = await axios.post(`${API_URL}/api/auth/signin`, {
     email,
     password,
   });
@@ -69,7 +71,7 @@ export const signIn = async ({ email, password }) => {
 };
 
 export const checkEmailDuplicate = async email => {
-  const { data } = await axios.post('/api/auth/signup/email', {
+  const { data } = await axios.post(`${API_URL}/api/auth/signup/email`, {
     email,
   });
 
@@ -77,7 +79,7 @@ export const checkEmailDuplicate = async email => {
 };
 
 export const signUp = async ({ email, name, password, phone, mainAddress, detailAddress, postcode }) => {
-  await axios.post('/api/auth/signup', {
+  await axios.post(`${API_URL}/api/auth/signup`, {
     email,
     name,
     phone,
@@ -89,22 +91,22 @@ export const signUp = async ({ email, name, password, phone, mainAddress, detail
 };
 
 export const addCoupon = async id => {
-  const { data } = await axios.post(`/api/coupons/${id}`);
+  const { data } = await axios.post(`${API_URL}/api/coupons/${id}`);
   return data;
 };
 
 export const addFavorite = async productId => {
-  const { data } = await axios.post('/api/favorites/me', { id: productId });
+  const { data } = await axios.post(`${API_URL}/api/favorites/me`, { id: productId });
   return data;
 };
 
 export const addCart = async ({ id, size }) => {
-  const { data } = await axios.post(`/api/carts/me/${id}`, { size });
+  const { data } = await axios.post(`${API_URL}/api/carts/me/${id}`, { size });
   return data;
 };
 
 export const addAddress = async ({ name, phone, mainAddress, detailAddress, postcode }) => {
-  const { data } = await axios.post('/api/users/me/address', {
+  const { data } = await axios.post(`${API_URL}/api/users/me/address`, {
     recipient: name,
     recipientPhone: phone,
     mainAddress,
@@ -116,25 +118,25 @@ export const addAddress = async ({ name, phone, mainAddress, detailAddress, post
 };
 
 export const order = async form => {
-  await axios.post('/api/order/pay', form);
+  await axios.post(`${API_URL}/api/order/pay`, form);
 };
 
 export const changeCartQuantity = async ({ id, size, quantity }) => {
-  await axios.patch(`/api/carts/me/${id}`, { size, quantity });
+  await axios.patch(`${API_URL}/api/carts/me/${id}`, { size, quantity });
 };
 
 export const changeDefaultAddress = async id => {
-  await axios.patch(`/api/users/me/address/default/${id}`);
+  await axios.patch(`${API_URL}/api/users/me/address/default/${id}`);
 };
 
 export const removeCart = async id => {
-  await axios.delete(`/api/carts/me/${id}`);
+  await axios.delete(`${API_URL}/api/carts/me/${id}`);
 };
 
 export const removeAddress = async id => {
-  await axios.delete(`/api/users/me/address/${id}`);
+  await axios.delete(`${API_URL}/api/users/me/address/${id}`);
 };
 
 export const removeFavorite = async favoriteId => {
-  await axios.delete(`/api/favorites/me/${favoriteId}`);
+  await axios.delete(`${API_URL}/api/favorites/me/${favoriteId}`);
 };
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,17 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import jsconfigPaths from 'vite-jsconfig-paths';
 
-const PORT = 8000;
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), jsconfigPaths()],
-  server: {
-    proxy: {
-      '/api': {
-        target: `http://ec2-3-36-56-62.ap-northeast-2.compute.amazonaws.com:${PORT}/`,
-        changeOrigin: true,
-      },
-    },
-  },
 });
